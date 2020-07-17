d438201e8590a976922a622886576f0d2eea24f6
Diego Sanchez <diegoroj3@gmail.com>
2020-07-17 11:54:27 -0500

    se arreglo la manera de representar la tabla de datos en la prueba de series

--- punto 3/prueba-serie/series.py
+++ punto 3/prueba-serie/series.py
@@ -50,15 +50,32 @@ def main():
         int_sup += incr
     table_chi = ((FE - fo) ** 2) / FE
     chi_cal = np.sum(table_chi)
-    print("frecuencia obtenida:\n") 
-    print("0.0-0.1 | 0.1-0.2 | 0.2-0.3 | 0.3-0.4 | 0.4-0.5 | 0.5-0.6 | 0.6-0.7 | 0.7-0.8 | 0.8-0.9 | 0.9-1.0 ")
+    file = open("Table.txt","w+")
+    file.write("frecuencia obtenida:\n") 
+    file.write("           |0.0-0.1 | 0.1-0.2 | 0.2-0.3 | 0.3-0.4 | 0.4-0.5 | 0.5-0.6 | 0.6-0.7 | 0.7-0.8 | 0.8-0.9 | 0.9-1.0|\n")
+    int_inf = 0.0
+    int_sup = 1/inc
+    incr = 1 / inc
+   
     for i in range(10):
-        print(f'    {fo[i,0]} | {fo[i,1]}     |   {fo[i,2]}   |   {fo[i,3]}   |   {fo[i,4]}   |   {fo[i,5]}   |   {fo[i,6]}   |   {fo[i,7]}   |   {fo[i,8]}   |   {fo[i,9]}  ')
-    print("(fe-fo)²/fe\n")
-    print("0.0-0.1 | 0.1-0.2 | 0.2-0.3 | 0.3-0.4 | 0.4-0.5 | 0.5-0.6 | 0.6-0.7 | 0.7-0.8 | 0.8-0.9 | 0.9-1.0 ")
+        
+        text_temp = f"{str(int_inf)[:3]} - {str(int_sup)[:3]}  |   {int(fo[i,0])}  |   {int(fo[i,1])}   |   {int(fo[i,2])}   |   {int(fo[i,3])}   |   {int(fo[i,4])}   |   {int(fo[i,5])}   |   {int(fo[i,6])}   |   {int(fo[i,7])}   |   {int(fo[i,8])}   |   {int(fo[i,9])}  |"
+        file.write(text_temp + "\n")
+        int_inf = int_sup
+        int_sup += incr
+        
+    file.write("(fe-fo)²/fe:\n")
+    file.write("           |  0.0-0.1 |   0.1-0.2 |   0.2-0.3 |   0.3-0.4 |   0.4-0.5 |   0.5-0.6 |   0.6-0.7 |   0.7-0.8 |   0.8-0.9 |   0.9-1.0|\n")
+    int_inf = 0.0
+    int_sup = 1/inc
+    incr = 1 / inc
+   
     for i in range(10):
-        print(f'    {table_chi[i,0]} | {table_chi[i,1]} |   {table_chi[i,2]}   |   {table_chi[i,3]}   |   {table_chi[i,4]}   |   {table_chi[i,5]}   |   {table_chi[i,6]}   |   {table_chi[i,7]}   |   {table_chi[i,8]}   |   {table_chi[i,9]}  ')
-    
+        
+        text_temp = f"{str(int_inf)[:3]} - {str(int_sup)[:3]}  |   {int(table_chi[i,0])}  |   {int(table_chi[i,1])}   |   {int(table_chi[i,2])}   |   {int(table_chi[i,3])}   |   {int(table_chi[i,4])}   |   {int(table_chi[i,5])}   |   {int(table_chi[i,6])}   |   {int(table_chi[i,7])}   |   {int(table_chi[i,8])}   |   {int(table_chi[i,9])}  |"
+        file.write(text_temp + "\n")
+        int_inf = int_sup
+        int_sup += incr
     print("chi calculado:",chi_cal)
     chi_cri = 124.3421
     
